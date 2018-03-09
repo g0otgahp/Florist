8c6921879c65077179c1dafa39f179dc50e76251
Jay <tuna_sang@hotmail.com>
2018-03-09 12:41:19 +0700

    Initial commit

---
+++ application/views/homepage/abountus.php
@@ -0,0 +1,36 @@
+<div class="col-sm-9 blog-main">
+  <div class="panel panel-default">
+    <div class="panel-heading">เกี่ยวกับเรา</div>
+    <div class="panel-body">
+      <div class="row" align="center">
+        <div><b>เกี่ยวกับเรา</b></div>
+
+        <div>
+          ร้านค้าดอกไม้ออนไลน์
+        </div>
+        <div>
+          สั่งซื้อสะดวก เช็คของออนไลน์ ง่ายนิดเดียว
+        </div>
+
+        <div>
+          เจ้าของร้าน <b>สุดารัตน์ กำเนิดบุญ</b>
+        </div>
+
+        <hr>
+
+        <div><b>ติดต่อเรา</b></div>
+
+        <div>เบอร์โทรศัพท์</div>
+        <span style="color:red;">
+          <div>089-4536547</div>
+        </span>
+        <br>
+        <div>E-mail</div>
+
+        <div>Acc.seoul@gmail.com</div>
+
+
+      </div>
+    </div>
+  </div>
+</div>
